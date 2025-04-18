0c7910f997da4e53809c8bc6950d685a417f5e6f
500lbs <stevenksteffen@gmail.com>
2025-04-18 15:09:38 -0400

    first push

--- brake.gd
+++
@@ -1,7 +0,0 @@
-class_name Brake
-extends Resource
-
-
-@export_range(0.0, 10000.0, 0.1, "or_greater") var max_torque := 5000.0
-
-@export_range(0.0, 3000.0, 0.1, "or_greater") var max_handbrake_torque := 1600.0
--- camera.gd
+++
@@ -1,9 +0,0 @@
-extends Camera3D
-
-
-@export var target : Node3D
-
-
-func _process(_delta : float) -> void:
-	if target != null:
-		look_at(target.global_position)
